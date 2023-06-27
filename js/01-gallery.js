d89595cf10c925086ced46f6e46d43e60d768237
Proofwolf <65850243+Proofwolf@users.noreply.github.com>
2023-06-27 20:05:54 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,28 +1,54 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const galleryEl = document.querySelector('.gallery');
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItemsMarkup(galleryItems);
 
-const galleryMarkup = galleryItems
-  .map(
-    ({ preview, original, description }) =>
-      `<div class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</div>`
-  )
-  .join('');
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryContainer.addEventListener("click", onImageClick);
 
-galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
+function createGalleryItemsMarkup(items) {
+    return galleryItems
+    .map(({ preview, original, description}) => {
+        return `
+            <div class="gallery__item">
+                <a class="gallery__link" href="${original}" rel='noreferrer noopener nofollow'>
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                    />
+                </a>
+            </div> 
+        `;
+    })
+    .join('');
+};
 
-galleryEl.addEventListener('click', event => {
-  event.preventDefault();
+function onImageClick(event) {
+    event.preventDefault();
+
+    if(!event.target.classList.contains('gallery__image')) {
+        return;
+    }
+
+    const instance = basicLightbox.create(`
+    <img src='${event.target.dataset.source}'>
+    `);
+
+    instance.show();
+
+    window.addEventListener('keydown', onEscapePress);
+
+    function onEscapePress(event) {
+        if (event.code === 'Escape') {
+        instance.close();
+
+        window.removeEventListener('keydown', event);
+        }
+    };
+};
   if (event.target.nodeName !== 'IMG') {
     return;
   }
